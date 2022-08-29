2d993f4e3d4decfb964425d7b5ad3056f6554f04
asd-ux-ux <74491743+asd-ux-ux@users.noreply.github.com>
2022-08-29 12:00:01 +0500

    telegram

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello, World!</h1>');
+})
+
+server.listen(8443);
+
+module.exports = server;
