294a59cba9241600e915e332257fec46bf79addb
asd-ux-ux <74491743+asd-ux-ux@users.noreply.github.com>
2022-08-29 13:47:45 +0500

    telegram

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  let data = '';
-  req.on('data', chunk => {
-    data += chunk;
-  });
-  req.on('end', () => {
-    console.log(JSON.parse(data)); // 'Buy the milk'
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<h1>Hello, World!</h1>');
-  });
 })
 
 server.listen(8443);
